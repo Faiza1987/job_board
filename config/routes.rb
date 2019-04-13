c2a6498a778efcffc5b2def5c5811960cce0b4b2
Faiza Husain <faiza.ahsan1222@gmail.com>
2019-04-13 16:51:35 -0700

    Add rounte, controller, and view for the new action

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get '/jobs', to: 'jobs#index', as: 'jobs'
+
+  get '/jobs/new', to: 'jobs#new', as: 'new_job'
+
   get '/jobs/:id', to: 'jobs#show', as: 'job'
+
 end
