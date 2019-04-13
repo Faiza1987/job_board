c004d5eed289fd1298d70fdd6a2922efc1543936
Faiza Husain <faiza.ahsan1222@gmail.com>
2019-04-13 16:32:36 -0700

    Added the show action and view

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -2,4 +2,8 @@ class JobsController < ApplicationController
   def index 
     @jobs = Job.all.order(:id)
   end
+
+  def show 
+    @job = Job.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get '/jobs', to: 'jobs#index', as: 'jobs'
+  get '/jobs/:id', to: 'jobs#show', as: 'job'
 end
